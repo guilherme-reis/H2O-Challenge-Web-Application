451edf4e8f2d46870dca5a5df247e7c060169ee7
Guilherme Reis <33372432+guilherme-reis@users.noreply.github.com>
2025-02-13 20:32:40 -0800

    Fix missing drinkWater function

--- script.js
+++ script.js
@@ -68,3 +68,19 @@ function updateBigCup() {
         liters.innerText = `${1 - (50 * fullCups / 1000)}L`
     }
 }
+function drinkWater(idx) {
+    if (idx === 7 && smallCups[idx].classList.contains("full")) idx--;
+    else if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--;
+    }
+
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full');
+        } else {
+            cup.classList.remove('full');
+        }
+    });
+
+    updateBigCup();
+}
